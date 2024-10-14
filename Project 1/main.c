c446b705df6fc9934824784c13138f50a5d6ff11
Saad Ahmed <saadahmd7223@gmail.com>
2024-10-14 18:54:41 +0530

    1st C learning

---
+++ Project 1/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    // Initialize random number generator
+    srand(time(0));
+
+    // Generate a random number between 1 and 100
+    int randomNumber = (rand() % 100) + 1;
+    int no_of_guesses = 0;
+    int guessed;
+
+    // Print the random number
+    // printf("Random Number: %d\n", randomNumber);
+
+    do
+    {
+        printf("Guess the number: ");
+        scanf("%d", &guessed);
+        if(guessed>randomNumber){
+            printf("Lower number please\n");
+        }
+        else if(guessed<randomNumber){
+            printf("Higher number please\n");
+        }
+        else{
+            printf("Congrats!!\n");
+        }
+        no_of_guesses++;
+
+    } while (guessed != randomNumber);
+
+    printf("You guessed the number in %d guesses", no_of_guesses);
+
+    return 0;
+}
